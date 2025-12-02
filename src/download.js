401e2649796fb656c8e8cdaea8aeb0f2b3abfdee
Sean Baker <2939188+seanrbaker@users.noreply.github.com>
2025-12-02 19:24:21 +0100

    Dynamic pull of the upstream application. Until they break it again!

--- src/download.js
+++ src/download.js
@@ -19,4 +19,45 @@ async function downloadFile(url, destPath) {
   });
 }
 
-module.exports = { downloadFile };
+async function getLatestVersionUrl(baseUrl = 'https://dl.dod.cyber.mil/wp-content/uploads/stigs/zip/U_STIGViewer-linux_x64-') {
+  let major = 3;
+  let minor = 6;
+  let patch = 0;
+  let latestUrl = `${baseUrl}${major}-${minor}-${patch}.zip`;
+
+  console.log(`Checking for newer versions starting from ${major}.${minor}.${patch}...`);
+
+  while (true) {
+    // Check next patch version
+    const nextPatchUrl = `${baseUrl}${major}-${minor}-${patch + 1}.zip`;
+    // Check next minor version
+    const nextMinorUrl = `${baseUrl}${major}-${minor + 1}-0.zip`;
+
+    try {
+      // Try next patch first
+      await axios.head(nextPatchUrl);
+      patch++;
+      latestUrl = nextPatchUrl;
+      console.log(`Found newer version: ${major}.${minor}.${patch}`);
+      continue;
+    } catch (e) {
+      // If patch fails, try minor
+      try {
+        await axios.head(nextMinorUrl);
+        minor++;
+        patch = 0;
+        latestUrl = nextMinorUrl;
+        console.log(`Found newer version: ${major}.${minor}.${patch}`);
+        continue;
+      } catch (e2) {
+        // Both failed, we have the latest
+        break;
+      }
+    }
+  }
+
+  console.log(`Latest version found: ${major}.${minor}.${patch}`);
+  return latestUrl;
+}
+
+module.exports = { downloadFile, getLatestVersionUrl };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const path = require('path');
 const fs = require('fs');
-const { downloadFile } = require('./download');
+const { downloadFile, getLatestVersionUrl } = require('./download');
 const { extractZip, findAppAsar } = require('./extract');
 const { buildApp } = require('./build');
 
-const URL = 'https://dl.dod.cyber.mil/wp-content/uploads/stigs/zip/U_STIGViewer-linux_x64-3-6-0.zip';
+// const URL = 'https://dl.dod.cyber.mil/wp-content/uploads/stigs/zip/U_STIGViewer-linux_x64-3-6-0.zip';
 const WORK_DIR = path.join(__dirname, '../work');
 const DOWNLOAD_PATH = path.join(WORK_DIR, 'stig-viewer.zip');
 const EXTRACT_DIR = path.join(WORK_DIR, 'extracted');
@@ -16,7 +16,8 @@ async function main() {
 
     console.log('Downloading...');
     if (!fs.existsSync(DOWNLOAD_PATH)) {
-        await downloadFile(URL, DOWNLOAD_PATH);
+        const url = await getLatestVersionUrl();
+        await downloadFile(url, DOWNLOAD_PATH);
     } else {
         console.log('File already downloaded.');
     }
